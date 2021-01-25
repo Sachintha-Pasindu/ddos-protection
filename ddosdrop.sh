42c019ef20e889060cf255e463626a4de812218c
Sachintha Pasindu <sachinthakarunarathne28@gmail.com>
2021-01-25 21:58:09 +0530

    Create ddosdrop.sh
    hsenid

---
+++ ddosdrop.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+iptables -A FORWARD -j DROP
+iptables -A OUTPUT -j ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j DROP
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport http -j ACCEPT
+iptables -A INPUT -p tcp --dport https -j ACCEPT
+
+iptables -N specialips
+iptables -A specialips -s xxx.xxx.xxx.xxx -j RETURN  #IP addresses you want to accept packets from
+iptables -A specialips -s yyy.yyy.yyy.yyy -j RETURN  
+iptables -A specialips -j DROP
+
+iptables -A INPUT -j specialips
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT           
+iptables -A INPUT -j DROP
+
+iptables-save > /etc/iptables.rules
